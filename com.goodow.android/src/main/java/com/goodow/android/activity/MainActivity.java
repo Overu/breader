694fd3e22b843a22230a01f9f1b7edb2201ab46b
xiahang@goodow.com <xiahang@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-03-21 07:20:05 +0000

    git-svn-id: https://goodow.googlecode.com/svn@18 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- com.goodow.android/src/main/java/com/goodow/android/activity/MainActivity.java
+++ com.goodow.android/src/main/java/com/goodow/android/activity/MainActivity.java
@@ -1,15 +1,14 @@
 package com.goodow.android.activity;
 
-import com.goodow.android.persist.Persistor;
-import com.google.inject.Inject;
-
-import com.retech.reader.chassis.R;
-
+import roboguice.activity.RoboActivity;
 import android.os.Bundle;
 import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import roboguice.activity.RoboActivity;
+
+import com.goodow.android.R;
+import com.goodow.android.persist.Persistor;
+import com.google.inject.Inject;
 
 public class MainActivity extends RoboActivity {
 
@@ -22,7 +21,7 @@ public class MainActivity extends RoboActivity {
     setContentView(R.layout.main);
     WebView webView = (WebView) findViewById(R.id.webview);
     WebSettings webSettings = webView.getSettings();
-    webSettings.setJavaScriptEnabled(true);
+    webSettings.setJavaScriptEnabled(true); 
     webView.setWebChromeClient(new WebChromeClient());
     webView.addJavascriptInterface(persistor, "Persistor");
     webView
