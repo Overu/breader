794cb4f84809b05a0d5237124c16bd6a36b32c76
xiahang@goodow.com <xiahang@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-03-22 09:52:08 +0000

    git-svn-id: https://goodow.googlecode.com/svn@33 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- com.goodow.android/src/main/java/com/goodow/android/App.java
+++ com.goodow.android/src/main/java/com/goodow/android/App.java
@@ -8,49 +8,60 @@ import android.content.Intent;
 import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
-import android.webkit.WebChromeClient;
+import android.view.Window;
+import android.view.WindowManager;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
-import android.webkit.WebViewClient;
 
 public class App extends DroidGap {
 	// monitor platform changes
 	private IntentFilter mNetworkStateChangedFilter;
 	private BroadcastReceiver mNetworkStateIntentReceiver;
-	//default assign cache size 8MB
+	// default assign cache size 8MB
 	private final int CACHE_SIZE = 1024 * 1024 * 5;
+	private WebView view = null;
 
-	@Override 
+	@Override
 	public void onCreate(final Bundle arg0) {
 		super.onCreate(arg0);
-		
-		//setting the cache path
-		String cachePath = this.getApplicationContext().getDir("cache", Context.MODE_PRIVATE).getPath(); 
-		
+		// setting full screnn
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		// setting the cache path
+		String cachePath = this.getApplicationContext()
+				.getDir("cache", Context.MODE_PRIVATE).getPath();
+
 		this.loadUrlTimeoutValue = 0;
-		super.loadUrl("http://dev.goodow.com/"); 
-		
-		WebView view = this.appView;
-		view.getSettings().setAppCacheEnabled(true); 
+		super.loadUrl("http://dev.goodow.com/");
+
+		view = this.appView;
+		view.getSettings().setAppCacheEnabled(true);
 		view.getSettings().setAppCacheMaxSize(CACHE_SIZE);
 		view.getSettings().setAppCachePath(cachePath);
 		view.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
 		
-//		WebView view = new WebView(this);
-//		WebSettings settings = view.getSettings(); 
+		
+		String dir = this.getApplicationContext().getDir("database", Context.MODE_PRIVATE).getPath();   
+		view.getSettings().setDatabaseEnabled(true);     
+		view.getSettings().setDatabasePath(dir);   
+		view.getSettings().setDomStorageEnabled(true);   
 
-		view.getSettings().setJavaScriptEnabled(true);
-		view.getSettings().setAppCacheEnabled(true);
-//		
-//		WebChromeClient chromeClient = new WebChromeClient();
-//		view.setWebChromeClient(new WebChromeClient());
-//		view.setWebViewClient(new WebViewClient());  
-//		view.setNetworkAvailable(true); 
-//
-		registratNetworkChanges(view); 
-//
-//		view.loadUrl("file:///android_asset/www/NewFile.html");
-//		this.setContentView(view);
+		// WebView view = new WebView(this);
+		// WebSettings settings = view.getSettings();
+		//
+		// view.getSettings().setJavaScriptEnabled(true);
+		// view.getSettings().setAppCacheEnabled(true);
+		//
+		// WebChromeClient chromeClient = new WebChromeClient();
+		// view.setWebChromeClient(new WebChromeClient());
+		// view.setWebViewClient(new WebViewClient());
+		// view.setNetworkAvailable(true);
+		//
+		registratNetworkChanges(view);
+		//
+		// view.loadUrl("file:///android_asset/www/NewFile.html");
+		// this.setContentView(view);
 	}
 
 	private void registratNetworkChanges(final WebView view) {
@@ -72,7 +83,7 @@ public class App extends DroidGap {
 			}
 		};
 
-		registerReceiver(mNetworkStateIntentReceiver,
+		this.registerReceiver(mNetworkStateIntentReceiver,
 				mNetworkStateChangedFilter);
 	}
 }
