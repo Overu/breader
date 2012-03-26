d133a6682b26b8321096b1301dc33f6fbf8de6bc
i@goodow.com <i@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-03-26 05:45:35 +0000

    git-svn-id: https://goodow.googlecode.com/svn@118 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- com.goodow.web/trunk/com.goodow.web.war/src/main/scripts/mobile.sh
+++ com.goodow.web/trunk/com.goodow.web.war/src/main/scripts/mobile.sh
@@ -1,4 +1,4 @@
 pwd
-cd mobile
-svn add * --force
-svn --username $username.subversion.mobile --password $password.subversion.mobile ci -m ""
+svn rm https://goodow.googlecode.com/svn/com.goodow.web/trunk/com.goodow.web.mobile/ -m "" --username dev@goodow.com --password gn7aS3Sp9hS4 --force
+svn import mobile https://goodow.googlecode.com/svn/com.goodow.web/trunk/com.goodow.web.mobile/ -m "" --username dev@goodow.com --password gn7aS3Sp9hS4 --force
+rm -r mobile
