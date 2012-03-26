a3295d2dbf46f3bb24af12833f03081947d9a7bb
i@goodow.com <i@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-03-26 05:46:17 +0000

    git-svn-id: https://goodow.googlecode.com/svn@119 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- com.goodow.web/trunk/com.goodow.web.war/src/main/scripts/mobile.sh
+++ com.goodow.web/trunk/com.goodow.web.war/src/main/scripts/mobile.sh
@@ -1,4 +1,4 @@
 pwd
 svn rm https://goodow.googlecode.com/svn/com.goodow.web/trunk/com.goodow.web.mobile/ -m "" --username dev@goodow.com --password gn7aS3Sp9hS4 --force
 svn import mobile https://goodow.googlecode.com/svn/com.goodow.web/trunk/com.goodow.web.mobile/ -m "" --username dev@goodow.com --password gn7aS3Sp9hS4 --force
-rm -r mobile
+rm -rf mobile
