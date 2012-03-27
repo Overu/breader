8d1080cf54fcf7ea5b01dc58ca817630da0f6ca0
chen@goodow.com <chen@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-03-27 02:31:48 +0000

    git-svn-id: https://goodow.googlecode.com/svn@166 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/WavePanel.java
+++ com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/WavePanel.java
@@ -4,6 +4,9 @@ import com.google.gwt.core.client.GWT;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.CssResource.NotStrict;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.ImageResource.ImageOptions;
+import com.google.gwt.resources.client.ImageResource.RepeatStyle;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
@@ -11,6 +14,9 @@ import com.google.gwt.user.client.ui.Widget;
 public class WavePanel extends FlowPanel {
 
   interface Bundle extends ClientBundle {
+    @ImageOptions(repeatStyle = RepeatStyle.Horizontal)
+    ImageResource bodyBackgroundTop();
+
     @NotStrict
     @Source("waveClean.css")
     CssResource clean();
