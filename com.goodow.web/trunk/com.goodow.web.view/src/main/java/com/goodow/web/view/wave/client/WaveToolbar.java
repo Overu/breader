ecf5e0e6c44b10e9b8107e85c6ab01dbbc80e3bc
tianfengya@goodow.com <tianfengya@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-03-28 05:53:37 +0000

    git-svn-id: https://goodow.googlecode.com/svn@197 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/WaveToolbar.java
+++ com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/WaveToolbar.java
@@ -1,23 +1,52 @@
 package com.goodow.web.view.wave.client;
 
 import com.google.gwt.core.client.GWT;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.resources.client.ImageResource.ImageOptions;
+import com.google.gwt.resources.client.ImageResource.RepeatStyle;
+import com.google.gwt.user.client.ui.FlowPanel;
 
-public class WaveToolbar extends Composite {
-  interface ToolbarUiBinder extends UiBinder<Widget, WaveToolbar> {
+public class WaveToolbar extends FlowPanel {
+  interface Resources extends ClientBundle {
+    @Source("WaveToolbar.css")
+    Style style();
+
+    @ImageOptions(repeatStyle = RepeatStyle.Horizontal)
+    ImageResource waveToolbarActive();
+
+    @ImageOptions(repeatStyle = RepeatStyle.Horizontal)
+    ImageResource waveToolbarEmpty();
+
+  }
+  interface Style extends CssResource {
+    String waveToolbar();
   }
 
-  private static ToolbarUiBinder uiBinder = GWT.create(ToolbarUiBinder.class);
+  static final Resources res = GWT.create(Resources.class);
+  static {
+    res.style().ensureInjected();
+  }
 
   public WaveToolbar() {
-    initWidget(uiBinder.createAndBindUi(this));
+    addStyleName(res.style().waveToolbar());
   }
 
+  /**
+   * 
+   * Create the ToolbarButton
+   */
   public ToolbarClickButton addClickButton() {
     ToolbarClickButton toolbarClickButton = new ToolbarClickButton();
-    getElement().appendChild(toolbarClickButton.getElement());
+    this.add(toolbarClickButton);
     return toolbarClickButton;
+  }
+
+  /**
+   * Create the ToolbarToggleButton
+   */
+  public ToolbarToggleButton addToggleButton() {
+    return null;
   }
 }
