de06cafe590ba9f17d27a312f334061564178d80
chenyupeng@goodow.com <chenyupeng@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-03-31 09:51:57 +0000

    git-svn-id: https://goodow.googlecode.com/svn@271 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- org.cloudlet.web/trunk/com.retech.reader.web/src/main/java/com/retech/reader/web/client/home/BookListPanel.java
+++ org.cloudlet.web/trunk/com.retech.reader.web/src/main/java/com/retech/reader/web/client/home/BookListPanel.java
@@ -1,23 +1,36 @@
 package com.retech.reader.web.client.home;
 
+import com.goodow.web.view.wave.client.ToolbarClickButton;
+import com.goodow.web.view.wave.client.WavePanel;
+import com.goodow.web.view.wave.client.WaveToolbar;
+
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.dom.client.Element;
 import com.google.gwt.dom.client.StyleInjector;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.place.shared.PlaceController;
 import com.google.gwt.resources.client.ClientBundle;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.CssResource.Shared;
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.uibinder.client.UiBinder;
 import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 import com.google.inject.Singleton;
 
-import com.retech.reader.web.shared.rpc.ReaderFactory;
+import com.retech.reader.web.client.mobile.ui.bar.SettingsView;
+import com.retech.reader.web.client.mobile.ui.talk.TalkView;
 
 import org.cloudlet.web.mvp.shared.ActivityAware;
 import org.cloudlet.web.mvp.shared.ActivityState;
 import org.cloudlet.web.mvp.shared.BasePlace;
+
+import java.util.logging.Logger;
 
 @Singleton
 public class BookListPanel extends Composite implements ActivityAware {
@@ -29,35 +42,97 @@ public class BookListPanel extends Composite implements ActivityAware {
     @Source("BookListPanel.portrait.css")
     Style portrait();
   }
+  interface Resources extends ClientBundle {
 
+    ImageResource refresh();
+
+    ImageResource search();
+
+    ImageResource settings();
+
+    ImageResource share();
+  }
   @Shared
   interface Style extends CssResource {
     String root();
   }
 
+  private Logger logger = Logger.getLogger(getClass().getName());
+  private static Resources res = GWT.create(Resources.class);
   private static Binder binder = GWT.create(Binder.class);
   private static Bundle bundle = GWT.create(Bundle.class);
 
-  private ReaderFactory f;
   @UiField(provided = true)
   BookFlip bookFlipOfBookListPanel;
   @UiField(provided = true)
   LibraryView libraryView;
-
-  private final Provider<BasePlace> places;
+  @UiField(provided = true)
+  WavePanel waveToolbar;
 
   @Inject
-  BookListPanel(final ReaderFactory f, final Provider<BasePlace> places,
-      final BookFlip bookFlipOfBookListPanel, final LibraryView libraryView) {
-    this.f = f;
-    this.places = places;
+  BookListPanel(final Provider<BasePlace> places, final BookFlip bookFlipOfBookListPanel,
+      final LibraryView libraryView, final WavePanel waveToolbar,
+      final PlaceController placeController, WaveToolbar toolbar) {
     this.bookFlipOfBookListPanel = bookFlipOfBookListPanel;
     this.libraryView = libraryView;
+    this.waveToolbar = waveToolbar;
+    toolbar = waveToolbar.toolbar();
+
+    ToolbarClickButton settingButton = toolbar.addClickButton();
+    settingButton.setText("设置");
+    settingButton.setVisualElement(createIcon(res.settings()));
+
+    ToolbarClickButton refreshButton = toolbar.addClickButton();
+    refreshButton.setText("刷新");
+    refreshButton.setVisualElement(createIcon(res.refresh()));
+
+    ToolbarClickButton shareButton = toolbar.addClickButton();
+    shareButton.setText("分享");
+    shareButton.setVisualElement(createIcon(res.share()));
+
+    ToolbarClickButton searchButton = toolbar.addClickButton();
+    searchButton.setText("搜索");
+    searchButton.setVisualElement(createIcon(res.search()));
     initWidget(binder.createAndBindUi(this));
     String portraitCss =
         "@media only screen and (max-width: 1000px) {" + bundle.portrait().getText() + "}";
     StyleInjector.injectAtEnd(portraitCss);
 
+    settingButton.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(final ClickEvent event) {
+        placeController.goTo(places.get().setPath(SettingsView.class.getName()));
+      }
+    });
+
+    refreshButton.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(final ClickEvent event) {
+        logger.info("开发中");
+      }
+    });
+
+    shareButton.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(final ClickEvent event) {
+        placeController.goTo(places.get().setPath(TalkView.class.getName()));
+      }
+    });
+
+    searchButton.addClickHandler(new ClickHandler() {
+      @Override
+      public void onClick(final ClickEvent event) {
+        logger.info("开发中");
+      }
+    });
+
+  }
+
+  /**
+   * Creates an icon Element.
+   */
+  public Element createIcon(final ImageResource imageResource) {
+    return AbstractImagePrototype.create(imageResource).createElement();
   }
 
   @Override
@@ -65,4 +140,5 @@ public class BookListPanel extends Composite implements ActivityAware {
     bookFlipOfBookListPanel.onStart(state);
     libraryView.onStart(state);
   }
+
 }
