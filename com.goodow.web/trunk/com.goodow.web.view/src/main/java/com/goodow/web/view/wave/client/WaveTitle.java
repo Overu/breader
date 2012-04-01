097ccf7dffb0b9eb5843168328286d5a31de89c4
tianfengya@goodow.com <tianfengya@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-04-01 05:47:28 +0000

    git-svn-id: https://goodow.googlecode.com/svn@300 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/WaveTitle.java
+++ com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/WaveTitle.java
@@ -2,27 +2,61 @@ package com.goodow.web.view.wave.client;
 
 import com.google.gwt.core.client.GWT;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.uibinder.client.UiBinder;
-import com.google.gwt.uibinder.client.UiField;
-import com.google.gwt.user.client.ui.Composite;
-import com.google.gwt.user.client.ui.Widget;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.resources.client.ImageResource;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
+import com.google.gwt.user.client.ui.FlowPanel;
 
-public class WaveTitle extends Composite {
+public class WaveTitle extends FlowPanel {
+  interface Resources extends ClientBundle {
+    @Source("waveTitle.css")
+    Style style();
 
-  interface WaveTitleUiBinder extends UiBinder<Widget, WaveTitle> {
+    ImageResource waveTitleReturn();
+  }
+  interface Style extends CssResource {
+    String waveTitle();
+
+    String waveTitleText();
   }
 
-  private static WaveTitleUiBinder uiBinder = GWT.create(WaveTitleUiBinder.class);
-  @UiField
-  Element text;
-  @UiField
-  Element icons;
+  private static final Resources res = GWT.create(Resources.class);
+  static {
+    res.style().ensureInjected();
+  }
 
   public WaveTitle() {
-    initWidget(uiBinder.createAndBindUi(this));
+    this.addStyleName(res.style().waveTitle());
+    IconButtonTemplate iconButton = this.addIconClickButton();
+    iconButton.setIconElement(AbstractImagePrototype.create(res.waveTitleReturn()).createElement());
+    iconButton.addClickHandler(new ClickHandler() {
+
+      @Override
+      public void onClick(final ClickEvent event) {
+        History.back();
+      }
+    });
   }
 
+  public IconButtonTemplate addIconClickButton() {
+    IconButtonTemplate iconButton = new IconButtonTemplate();
+    this.add(iconButton);
+    return iconButton;
+  }
+
+  /**
+   * 
+   * set the title bar title
+   */
   public void setText(final String text) {
-    this.text.setInnerText(text);
+    Element element = DOM.createDiv();
+    element.setInnerText(text);
+    element.addClassName(res.style().waveTitleText());
+    this.getElement().appendChild(element);
   }
 }
