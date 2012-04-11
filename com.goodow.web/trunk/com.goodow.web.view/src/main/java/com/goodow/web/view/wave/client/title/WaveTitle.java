82a557f0b84e2118854dc46353dd9723a72ca9da
i@goodow.com <i@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-04-11 03:39:01 +0000

    git-svn-id: https://goodow.googlecode.com/svn@438 a5ad12fa-7522-11de-87a4-b5b95bed8975

---
+++ com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/title/WaveTitle.java
@@ -0,0 +1,60 @@
+/*
+ * Copyright 2012 Goodow.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.goodow.web.view.wave.client.title;
+
+import com.goodow.web.view.wave.client.IconButtonTemplate;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.event.dom.client.ClickHandler;
+import com.google.gwt.user.client.DOM;
+import com.google.gwt.user.client.History;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
+import com.google.gwt.user.client.ui.FlowPanel;
+
+public class WaveTitle extends FlowPanel {
+
+  private Element text;
+
+  public WaveTitle() {
+    this.addStyleName(WaveTitleResources.css().waveTitle());
+    IconButtonTemplate iconButton = this.addIconClickButton();
+    iconButton.setIconElement(AbstractImagePrototype.create(
+        WaveTitleResources.image().waveTitleReturn()).createElement());
+    iconButton.addClickHandler(new ClickHandler() {
+
+      @Override
+      public void onClick(final ClickEvent event) {
+        History.back();
+      }
+    });
+
+    text = DOM.createSpan();
+    this.getElement().appendChild(text);
+  }
+
+  public IconButtonTemplate addIconClickButton() {
+    IconButtonTemplate iconButton = new IconButtonTemplate();
+    this.add(iconButton);
+    return iconButton;
+  }
+
+  /**
+   * 
+   * set the title bar title
+   */
+  public void setText(final String text) {
+    this.text.setInnerText(text);
+  }
+}
