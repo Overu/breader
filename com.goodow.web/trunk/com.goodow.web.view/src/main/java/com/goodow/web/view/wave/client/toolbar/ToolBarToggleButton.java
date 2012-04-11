1586c125658037458f26b3fdeda7723ce132afe3
i@goodow.com <i@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-04-11 03:44:55 +0000

    git-svn-id: https://goodow.googlecode.com/svn@443 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/toolbar/ToolBarToggleButton.java
+++ com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/toolbar/ToolBarToggleButton.java
@@ -11,7 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
-package com.goodow.web.view.wave.client.toolBar;
+package com.goodow.web.view.wave.client.toolbar;
 
 import com.google.gwt.user.client.ui.Composite;
 
