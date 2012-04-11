5cb7c088caca33845d4dd75f9725ac8294fba8c0
zhangwenjian@goodow.com <zhangwenjian@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-04-11 11:34:16 +0000

    git-svn-id: https://goodow.googlecode.com/svn@461 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- com.goodow.web/trunk/com.goodow.web.logging/src/main/java/com/goodow/web/logging/client/RpcThrowableReporter.java
+++ com.goodow.web/trunk/com.goodow.web.logging/src/main/java/com/goodow/web/logging/client/RpcThrowableReporter.java
@@ -1,3 +1,16 @@
+/*
+ * Copyright 2012 Goodow.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
 package com.goodow.web.logging.client;
 
 import com.goodow.web.logging.shared.RpcLoggingService;
@@ -9,7 +22,6 @@ import com.google.gwt.logging.client.LogConfiguration;
 import com.google.gwt.user.client.rpc.AsyncCallback;
 import com.google.inject.Singleton;
 
-
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
 import java.util.logging.Logger;
@@ -20,20 +32,21 @@ final class RpcThrowableReporter {
   private static class RpcThrowableReporterHandler implements UncaughtExceptionHandler {
     private final RpcLoggingServiceAsync service = GWT.create(RpcLoggingService.class);
 
-    public void onUncaughtException(Throwable e) {
+    @Override
+    public void onUncaughtException(final Throwable e) {
       LogRecord lr = new LogRecord(Level.FINE, "前台程序出现未处理异常");
       lr.setThrown(e);
       AsyncCallback<LogRecord> callback = new AsyncCallback<LogRecord>() {
 
         @Override
-        public void onFailure(Throwable caught) {
+        public void onFailure(final Throwable caught) {
           if (LogConfiguration.loggingIsEnabled()) {
             logger.log(Level.FINE, "解码前台异常时发生错误", caught);
           }
         }
 
         @Override
-        public void onSuccess(LogRecord result) {
+        public void onSuccess(final LogRecord result) {
           if (LogConfiguration.loggingIsEnabled()) {
             logger.log(result.getLevel(), result.getMessage(), result.getThrown());
           }
