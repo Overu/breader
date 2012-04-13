3e9c4646e251d00d405d74f112c0ab583476cc4b
fengjian@goodow.com <fengjian@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-04-13 09:05:05 +0000

    git-svn-id: https://goodow.googlecode.com/svn@508 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/WaveTest.java
+++ com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/WaveTest.java
@@ -17,12 +17,20 @@ import com.goodow.web.view.wave.client.panel.WavePanel;
 
 import com.google.gwt.core.client.JsArray;
 import com.google.gwt.dom.client.Touch;
+import com.google.gwt.event.dom.client.TouchEndEvent;
+import com.google.gwt.event.dom.client.TouchEndHandler;
+import com.google.gwt.event.dom.client.TouchMoveEvent;
+import com.google.gwt.event.dom.client.TouchMoveHandler;
 import com.google.gwt.event.dom.client.TouchStartEvent;
 import com.google.gwt.event.dom.client.TouchStartHandler;
-import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.History;
 import com.google.gwt.user.client.ui.HTMLPanel;
 
 public class WaveTest extends WavePanel {
+
+  private boolean start = false;
+  private int x1;
+  private int x2;
 
   public WaveTest() {
     // getWaveTitle().setText("test title");
@@ -42,12 +50,52 @@ public class WaveTest extends WavePanel {
 
       @Override
       public void onTouchStart(final TouchStartEvent event) {
+        event.preventDefault();
         JsArray<Touch> touches = event.getTouches();
         if (touches.length() == 2) {
-          Window.alert("sdfsafdsadf");
+          Touch touch1 = touches.get(0);
+          Touch touch2 = touches.get(1);
+          x1 = touch1.getPageX();
+          x2 = touch2.getPageX();
+          start = true;
         }
       }
     }, TouchStartEvent.getType());
+
+    hp.addDomHandler(new TouchMoveHandler() {
+
+      @Override
+      public void onTouchMove(final TouchMoveEvent event) {
+        event.preventDefault();
+        if (start) {
+          JsArray<Touch> touches = event.getTouches();
+          Touch touch1 = touches.get(0);
+          Touch touch2 = touches.get(1);
+          int touch1PageX = touch1.getPageX();
+          int touch2PageX = touch2.getPageX();
+          int left = touch1PageX - x1;
+          int right = touch2PageX - x2;
+          if (left > 0 && right > 0 && (left > 100 || right > 100)) {
+            History.forward();
+            start = false;
+            return;
+          } else if (left < 0 && right < 0 && (left < -100 || right < -100)) {
+            History.back();
+            start = false;
+            return;
+          }
+        }
+      }
+    }, TouchMoveEvent.getType());
+
+    hp.addDomHandler(new TouchEndHandler() {
+
+      @Override
+      public void onTouchEnd(final TouchEndEvent event) {
+        event.preventDefault();
+        start = false;
+      }
+    }, TouchEndEvent.getType());
 
   }
 }
