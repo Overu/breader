240b85e9fb54a68f20ae67c98bc1ead19fff598b
chenyupeng@goodow.com <chenyupeng@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-04-16 04:06:55 +0000

    git-svn-id: https://goodow.googlecode.com/svn@544 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- org.cloudlet.web/trunk/com.retech.reader.web/src/main/java/com/retech/reader/web/client/mobile/ui/TagsPanel.java
+++ org.cloudlet.web/trunk/com.retech.reader.web/src/main/java/com/retech/reader/web/client/mobile/ui/TagsPanel.java
@@ -18,6 +18,7 @@ import com.google.gwt.user.client.EventListener;
 import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.gwt.user.client.ui.Hyperlink;
 import com.google.gwt.user.client.ui.InlineHyperlink;
 import com.google.gwt.user.client.ui.TextBox;
@@ -29,35 +30,34 @@ public class TagsPanel extends Composite {
   }
 
   interface Style extends CssResource {
-
     String addTagPanel();
   }
 
-  private static final Binder binder = GWT.create(Binder.class);
+  private static Binder binder = GWT.create(Binder.class);
+
   @UiField
   Style style;
   @UiField
-  Widget root;
-  TextBox textBox;
+  HTMLPanel root;
   @UiField
   FlowPanel tagValue;
   @UiField
-  Button Buttoned;
-  private final WavePanel addTagPanel;
+  Button buttoned;
+
+  private final TextBox textBox;
 
   @Inject
   TagsPanel(final WavePanel addTagPanel) {
     initWidget(binder.createAndBindUi(this));
     addStyleName(WavePanelResources.css().waveFooter());
 
-    this.addTagPanel = addTagPanel;
     addTagPanel.addStyleName(style.addTagPanel());
     addTagPanel.getWaveTitle().setText("999");
     textBox = new TextBox();
     textBox.getElement().removeClassName("gwt-TextBox");
     addTagPanel.setWaveContent(textBox);
 
-    Element addTags = Buttoned.getElement().getPreviousSiblingElement();
+    Element addTags = buttoned.getElement().getPreviousSiblingElement();
     DOM.sinkEvents((com.google.gwt.user.client.Element) addTags, Event.ONCLICK);
     DOM.setEventListener((com.google.gwt.user.client.Element) addTags, new EventListener() {
       @Override
@@ -69,7 +69,8 @@ public class TagsPanel extends Composite {
           // addTagPanel.getElement().getStyle().setPadding(0, Unit.PX);
           // addTagPanel.getElement().getStyle().setPaddingBottom(2, Unit.PX);
           // addTagPanel.getElement().getStyle().setDisplay(Display.BLOCK);
-          getElement().appendChild(addTagPanel.getElement());
+          // getElement().appendChild(addTagPanel.getElement());
+          root.add(addTagPanel);
           textBox.setFocus(true);
         }
       }
@@ -77,13 +78,10 @@ public class TagsPanel extends Composite {
     textBox.addKeyDownHandler(new KeyDownHandler() {
       @Override
       public void onKeyDown(final KeyDownEvent event) {
-        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER && !textBox.equals("")) {
+        if (event.getNativeKeyCode() == KeyCodes.KEY_ENTER && !textBox.getText().trim().equals("")) {
           Hyperlink hyperLink = new InlineHyperlink(textBox.getValue(), "#");
           tagValue.add(hyperLink);
           textBox.setText("");
-          if (textBox.getValue().equals("") && event.getNativeKeyCode() == KeyCodes.KEY_ENTER) {
-            // popupPanel.hide();
-          }
         }
       }
     });
