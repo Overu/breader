4cf607c1d62c1697af2911939e305ede5de6f550
tianfengya@goodow.com <tianfengya@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-04-19 12:51:23 +0000

    git-svn-id: https://goodow.googlecode.com/svn@637 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- org.cloudlet.web/trunk/com.retech.reader.web/src/main/java/com/retech/reader/web/client/mobile/ui/UserStatus.java
+++ org.cloudlet.web/trunk/com.retech.reader.web/src/main/java/com/retech/reader/web/client/mobile/ui/UserStatus.java
@@ -1,20 +1,45 @@
 package com.retech.reader.web.client.mobile.ui;
 
+import com.goodow.web.view.wave.client.contact.UserStatusResources;
 import com.goodow.web.view.wave.client.panel.WavePanelResources;
 
 import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.ImageResource;
 import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.AbstractImagePrototype;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.FlowPanel;
+import com.google.gwt.user.client.ui.SimplePanel;
 import com.google.gwt.user.client.ui.Widget;
 
 public class UserStatus extends Composite {
   interface Binder extends UiBinder<Widget, UserStatus> {
   }
+  interface Resources extends ClientBundle {
+    ImageResource UserStatusUnknown();
+  }
 
+  private static Resources res = GWT.create(Resources.class);
   private Binder binder = GWT.create(Binder.class);
+  @UiField
+  FlowPanel users;
 
   public UserStatus() {
     initWidget(binder.createAndBindUi(this));
+    addUser(UserStatusResources.css().waveUserAvailable(), res.UserStatusUnknown());
+    addUser(UserStatusResources.css().waveUserIdle(), res.UserStatusUnknown());
+    addUser(UserStatusResources.css().waveUserInvisible(), res.UserStatusUnknown());
+    addUser(UserStatusResources.css().waveUserGroup(), res.UserStatusUnknown());
     addStyleName(WavePanelResources.css().waveHeader());
+  }
+
+  private void addUser(final String userStatusCss, final ImageResource imageResource) {
+    SimplePanel simplePanel = new SimplePanel();
+    simplePanel.add(AbstractImagePrototype.create(imageResource).createImage());
+    simplePanel.addStyleName(userStatusCss);
+    simplePanel.addStyleName(UserStatusResources.css().waveUser());
+    users.add(simplePanel);
   }
 }
