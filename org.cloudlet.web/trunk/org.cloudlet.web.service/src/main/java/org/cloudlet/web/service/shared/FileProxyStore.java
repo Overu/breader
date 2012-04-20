69dc601099359c53b900a3d7d4667da8a173a135
i@goodow.com <i@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-04-20 09:02:07 +0000

    git-svn-id: https://goodow.googlecode.com/svn@661 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- org.cloudlet.web/trunk/org.cloudlet.web.service/src/main/java/org/cloudlet/web/service/shared/FileProxyStore.java
+++ org.cloudlet.web/trunk/org.cloudlet.web.service/src/main/java/org/cloudlet/web/service/shared/FileProxyStore.java
@@ -1,60 +1,141 @@
 package org.cloudlet.web.service.shared;
 
-import com.google.gwt.storage.client.Storage;
+import com.goodow.web.feature.client.FeatureDetection;
+
+import com.google.gwt.core.client.Callback;
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
-import com.google.web.bindery.autobean.shared.Splittable;
-import com.google.web.bindery.autobean.shared.impl.StringQuoter;
-import com.google.web.bindery.requestfactory.shared.ProxyStore;
 
+import com.googlecode.gwtphonegap.client.file.DirectoryEntry;
+import com.googlecode.gwtphonegap.client.file.File;
+import com.googlecode.gwtphonegap.client.file.FileCallback;
+import com.googlecode.gwtphonegap.client.file.FileEntry;
+import com.googlecode.gwtphonegap.client.file.FileError;
+import com.googlecode.gwtphonegap.client.file.FileSystem;
+import com.googlecode.gwtphonegap.client.file.FileWriter;
+import com.googlecode.gwtphonegap.client.file.Flags;
+import com.googlecode.gwtphonegap.client.file.WriterCallback;
+
+import org.cloudlet.web.service.shared.rpc.ResourceProxy;
+
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
 @Singleton
-public class FileProxyStore implements ProxyStore {
-  private Storage storage;
-  String versionKey = "operationMessage.version";
+public class FileProxyStore {
+  private static final int SIZE = 500 * 1024 * 1024;
+
   private final static Logger logger = Logger.getLogger(FileProxyStore.class.getName());
 
-  private static final String EXPECTED_VERSION = "5";
+  private static native void requestQuota(int fileSystemType, int size,
+      Callback<Integer, Object> callback)/*-{
+                                                   $wnd.requestFileSystem  = window.requestFileSystem || window.webkitRequestFileSystem;
+                                                   var fail = function(error) {
+                                                   $entry(callback.@com.google.gwt.core.client.Callback::onFailure(Ljava/lang/Object;)(error));
+                                                   };
 
-  private static void errorHandler() {
+                                                   var suc = function(grantedBytes) {
+                                                   $entry(@org.cloudlet.web.service.shared.FileProxyStore::requestQuotaOnSuccess(ILcom/google/gwt/core/client/Callback;)(grantedBytes,callback));
+                                                   };
 
+                                                   $wnd.webkitStorageInfo.requestQuota(fileSystemType, size, $entry(suc), $entry(fail));
+
+                                                   }-*/;
+
+  private static void requestQuotaOnSuccess(final int size, final Callback<Integer, Object> callback) {
+    callback.onSuccess(size);
   }
+
+  private FileSystem fileSystem;
+  private DirectoryEntry rootDirectory;
+  private final File file;
+  private final Callback<Integer, Object> callback = new Callback<Integer, Object>() {
+
+    @Override
+    public void onFailure(final Object reason) {
+      logger.log(Level.SEVERE, "请求持久存储空间时出错(" + SIZE + " bytes)\n" + reason);
+    }
+
+    @Override
+    public void onSuccess(final Integer size) {
+      file.requestFileSystem(FileSystem.LocalFileSystem_PERSISTENT, size.intValue(),
+          new FileCallback<FileSystem, FileError>() {
+
+            @Override
+            public void onFailure(final FileError error) {
+              logger.log(Level.SEVERE, "获取文件系统时出错(ErrorCode=" + error.getErrorCode() + ")\n"
+                  + error);
+            }
+
+            @Override
+            public void onSuccess(final FileSystem entry) {
+              fileSystem = entry;
+              rootDirectory = fileSystem.getRoot();
+            }
+          });
+    }
+  };
+
+  private static final Flags flags = new Flags(true, false);
+
+  private final LocalStorage localStorage;
 
   @Inject
-  FileProxyStore() {
-    storage = Storage.getLocalStorageIfSupported();
+  FileProxyStore(final File file, final LocalStorage localStorage) {
+    this.file = file;
+    this.localStorage = localStorage;
 
-    String version = storage.getItem(versionKey);
-    if (version != null && !EXPECTED_VERSION.equals(version)) {
-      logger.warning("本地数据不兼容于新版本,将被清除");
-      storage.clear();
+    if (!FeatureDetection.mobileNative()) {
+      requestQuota(FileSystem.LocalFileSystem_PERSISTENT, SIZE, callback);
+    } else {
+      callback.onSuccess(SIZE);
     }
-    storage.setItem(versionKey, EXPECTED_VERSION);
   }
 
-  @Override
-  public Splittable get(final String key) {
-    Splittable toReturn = null;
-    // Splittable splittable = cache.get(key);
-    // if (splittable != null) {
-    // return splittable;
-    // }
-    String item = storage.getItem(key);
-    if (item != null) {
-      toReturn = StringQuoter.split(item);
-    }
-    return toReturn;
+  public DirectoryEntry getRootDirectory() {
+    return rootDirectory;
   }
 
-  @Override
-  public int nextId() {
-    return storage.getLength();
+  public void put(final String key, final ResourceProxy content,
+      final Callback<Void, Object> callback) {
+    rootDirectory.getFile(key, flags, new FileCallback<FileEntry, FileError>() {
+
+      @Override
+      public void onFailure(final FileError error) {
+        logger.log(Level.SEVERE, "出错了\n" + error);
+      }
+
+      @Override
+      public void onSuccess(final FileEntry fileEntry) {
+        fileEntry.createWriter(new FileCallback<FileWriter, FileError>() {
+
+          @Override
+          public void onFailure(final FileError error) {
+            logger.log(Level.SEVERE, "出错了\n" + error);
+          }
+
+          @Override
+          public void onSuccess(final FileWriter fileWriter) {
+            fileWriter.setOnErrorCallback(new WriterCallback<FileWriter>() {
+
+              @Override
+              public void onCallback(final FileWriter result) {
+                logger.log(Level.SEVERE, "出错了\n" + result);
+              }
+            });
+            fileWriter.setOnWriteEndCallback(new WriterCallback<FileWriter>() {
+
+              @Override
+              public void onCallback(final FileWriter result) {
+                callback.onSuccess(null);
+              }
+            });
+            String payload = localStorage.encode(content).getPayload();
+            fileWriter.write(payload);
+          }
+        });
+      }
+    });
   }
 
-  @Override
-  public void put(final String key, final Splittable value) {
-    // cache.put(key, value);
-    storage.setItem(key, value.getPayload());
-  }
 }
