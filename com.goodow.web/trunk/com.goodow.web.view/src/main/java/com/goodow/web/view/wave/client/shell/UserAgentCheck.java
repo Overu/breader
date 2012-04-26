2b3db649c525df188b607442593d09524294553d
chenyupeng@goodow.com <chenyupeng@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-04-26 03:03:24 +0000

    git-svn-id: https://goodow.googlecode.com/svn@692 a5ad12fa-7522-11de-87a4-b5b95bed8975

---
+++ com.goodow.web/trunk/com.goodow.web.view/src/main/java/com/goodow/web/view/wave/client/shell/UserAgentCheck.java
@@ -0,0 +1,28 @@
+package com.goodow.web.view.wave.client.shell;
+
+import com.goodow.web.view.wave.client.panel.WavePanel;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.CssResource;
+import com.google.gwt.uibinder.client.UiBinder;
+import com.google.gwt.uibinder.client.UiField;
+import com.google.gwt.user.client.ui.Widget;
+
+public class UserAgentCheck extends WavePanel {
+  interface Binder extends UiBinder<Widget, UserAgentCheck> {
+  }
+  interface Style extends CssResource {
+    String wavePanelRoot();
+  }
+
+  @UiField
+  Style style;
+
+  private static Binder binder = GWT.create(Binder.class);
+
+  public UserAgentCheck() {
+    this.getWaveTitle().setText("Google Wave – Your browser is not supported");
+    this.setWaveContent(binder.createAndBindUi(this));
+    this.addStyleName(style.wavePanelRoot());
+  }
+}
