f08e65b10ddd988321d9c49aa012d655c44cdbda
i@goodow.com <i@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-04-27 08:26:04 +0000

    git-svn-id: https://goodow.googlecode.com/svn@718 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- org.cloudlet.web/trunk/org.cloudlet.web.service/src/main/java/org/cloudlet/web/service/client/ioc/ServiceGinModule.java
+++ org.cloudlet.web/trunk/org.cloudlet.web.service/src/main/java/org/cloudlet/web/service/client/ioc/ServiceGinModule.java
@@ -5,6 +5,7 @@ import com.google.inject.Singleton;
 import com.google.web.bindery.requestfactory.shared.RequestTransport;
 
 import org.cloudlet.web.service.client.rpc.BaseRequestTransport;
+import org.cloudlet.web.service.shared.FileProxyStore;
 import org.cloudlet.web.service.shared.rpc.BaseReceiver;
 
 import java.util.logging.Logger;
@@ -18,7 +19,7 @@ public final class ServiceGinModule extends AbstractGinModule {
     logger.finest("config start");
 
     bind(RequestTransport.class).to(BaseRequestTransport.class).in(Singleton.class);
-
+    bind(FileProxyStore.class).asEagerSingleton();
     requestStaticInjection(BaseReceiver.class);
   }
 }
