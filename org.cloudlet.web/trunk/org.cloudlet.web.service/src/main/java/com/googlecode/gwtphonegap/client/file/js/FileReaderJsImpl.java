83e7e5b504de3cd765888fba7e26fb20c940ab93
i@goodow.com <i@goodow.com@a5ad12fa-7522-11de-87a4-b5b95bed8975>
2012-04-27 08:36:56 +0000

    code format
    git-svn-id: https://goodow.googlecode.com/svn@722 a5ad12fa-7522-11de-87a4-b5b95bed8975

--- org.cloudlet.web/trunk/org.cloudlet.web.service/src/main/java/com/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl.java
+++ org.cloudlet.web/trunk/org.cloudlet.web.service/src/main/java/com/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl.java
@@ -1,21 +1,20 @@
 /*
  * Copyright 2010 Daniel Kurka
  * 
- * Licensed under the Apache License, Version 2.0 (the "License"); you may not
- * use this file except in compliance with the License. You may obtain a copy of
- * the License at
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
  * 
  * http://www.apache.org/licenses/LICENSE-2.0
  * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
 package com.googlecode.gwtphonegap.client.file.js;
 
 import com.google.gwt.core.client.JavaScriptObject;
+
 import com.googlecode.gwtphonegap.client.file.FileEntry;
 import com.googlecode.gwtphonegap.client.file.FileError;
 import com.googlecode.gwtphonegap.client.file.FileReader;
@@ -23,119 +22,120 @@ import com.googlecode.gwtphonegap.client.file.ReaderCallback;
 
 public final class FileReaderJsImpl extends JavaScriptObject implements FileReader {
 
-	protected FileReaderJsImpl() {
+  private static void onCallback(final ReaderCallback<FileReader> callback,
+      final FileReaderJsImpl reader) {
+    callback.onCallback(reader);
+  }
 
-	}
+  protected FileReaderJsImpl() {
 
-	@Override
-	public native int getReadyState() /*-{
-		return this.readyState;
-	}-*/;
+  }
 
-	@Override
-	public native String getResult() /*-{
-		return this.result;
-	}-*/;
+  @Override
+  public native void abort()/*-{
+                            this.abort();
+                            }-*/;
 
-	@Override
-	public native FileError getError() /*-{
-		return this.error;
-	}-*/;
+  @Override
+  public native FileError getError() /*-{
+                                     return this.error;
+                                     }-*/;
 
-	@Override
-	public native void setOnLoadStartCallback(ReaderCallback<FileReader> callback) /*-{
-		var that = this;
-		var func = function() {
-			@com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
-		};
+  @Override
+  public native int getReadyState() /*-{
+                                    return this.readyState;
+                                    }-*/;
 
-		this.onloadstart = $entry(func);
+  @Override
+  public native String getResult() /*-{
+                                   return this.result;
+                                   }-*/;
 
-	}-*/;
+  @Override
+  public void readAsDataUrl(final FileEntry entry) {
+    FileEntryJsImpl entryJs = (FileEntryJsImpl) entry;
+    readAsDataUrl0(entryJs.getEntry());
 
-	@Override
-	public native void setOnProgressCallback(ReaderCallback<FileReader> callback) /*-{
-		var that = this;
-		var func = function() {
-			@com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
-		};
+  }
 
-		this.onprogress = $entry(func);
+  @Override
+  public void readAsText(final FileEntry entry) {
+    FileEntryJsImpl entryJs = (FileEntryJsImpl) entry;
+    readAsText0(entryJs.getEntry());
+  }
 
-	}-*/;
+  @Override
+  public native void setOnAbortCallback(ReaderCallback<FileReader> callback) /*-{
+                                                                             var that = this;
+                                                                             var func = function() {
+                                                                             @com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
+                                                                             };
 
-	@Override
-	public native void setOnloadCallback(ReaderCallback<FileReader> callback) /*-{
-		var that = this;
-		var func = function() {
-			@com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
-		};
+                                                                             this.onabort = $entry(func);
 
-		this.onload = $entry(func);
+                                                                             }-*/;
 
-	}-*/;
+  @Override
+  public native void setOnErrorCallback(ReaderCallback<FileReader> callback) /*-{
+                                                                             var that = this;
+                                                                             var func = function() {
+                                                                             @com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
+                                                                             };
 
-	@Override
-	public native void setOnAbortCallback(ReaderCallback<FileReader> callback) /*-{
-		var that = this;
-		var func = function() {
-			@com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
-		};
+                                                                             this.onerror = $entry(func);
 
-		this.onabort = $entry(func);
+                                                                             }-*/;
 
-	}-*/;
+  @Override
+  public native void setOnloadCallback(ReaderCallback<FileReader> callback) /*-{
+                                                                            var that = this;
+                                                                            var func = function() {
+                                                                            @com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
+                                                                            };
 
-	@Override
-	public native void setOnErrorCallback(ReaderCallback<FileReader> callback) /*-{
-		var that = this;
-		var func = function() {
-			@com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
-		};
+                                                                            this.onload = $entry(func);
 
-		this.onerror = $entry(func);
+                                                                            }-*/;
 
-	}-*/;
+  @Override
+  public native void setOnLoadEndCallback(ReaderCallback<FileReader> callback) /*-{
+                                                                               var that = this;
+                                                                               var func = function() {
+                                                                               @com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
+                                                                               };
 
-	@Override
-	public native void setOnLoadEndCallback(ReaderCallback<FileReader> callback) /*-{
-		var that = this;
-		var func = function() {
-			@com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
-		};
+                                                                               this.onloadend = $entry(func);
 
-		this.onloadend = $entry(func);
+                                                                               }-*/;
 
-	}-*/;
+  @Override
+  public native void setOnLoadStartCallback(ReaderCallback<FileReader> callback) /*-{
+                                                                                 var that = this;
+                                                                                 var func = function() {
+                                                                                 @com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
+                                                                                 };
 
-	private static void onCallback(ReaderCallback<FileReader> callback, FileReaderJsImpl reader) {
-		callback.onCallback(reader);
-	}
+                                                                                 this.onloadstart = $entry(func);
 
-	@Override
-	public native void abort()/*-{
-		this.abort();
-	}-*/;
+                                                                                 }-*/;
 
-	@Override
-	public void readAsDataUrl(FileEntry entry) {
-		FileEntryJsImpl entryJs = (FileEntryJsImpl) entry;
-		readAsDataUrl0(entryJs.getEntry());
+  @Override
+  public native void setOnProgressCallback(ReaderCallback<FileReader> callback) /*-{
+                                                                                var that = this;
+                                                                                var func = function() {
+                                                                                @com.googlecode.gwtphonegap.client.file.js.FileReaderJsImpl::onCallback(Lcom/googlecode/gwtphonegap/client/file/ReaderCallback;Lcom/googlecode/gwtphonegap/client/file/js/FileReaderJsImpl;)(callback, that);
+                                                                                };
 
-	}
+                                                                                this.onprogress = $entry(func);
 
-	private native void readAsDataUrl0(JavaScriptObject entry)/*-{
-		this.readAsDataURL(entry);
-	}-*/;
+                                                                                }-*/;
 
-	@Override
-	public void readAsText(FileEntry entry) {
-		FileEntryJsImpl entryJs = (FileEntryJsImpl) entry;
-		readAsText0(entryJs.getEntry());
-	}
+  private native void readAsDataUrl0(JavaScriptObject entry)/*-{
+                                                            this.readAsDataURL(entry);
+                                                            }-*/;
 
-	private native void readAsText0(JavaScriptObject entry)/*-{
-		this.readAsText(entry);
-	}-*/;
+  private native void readAsText0(JavaScriptObject entry)/*-{
+                                                         this.readAsText(entry);
+                                                         }-*/;
 
 }
