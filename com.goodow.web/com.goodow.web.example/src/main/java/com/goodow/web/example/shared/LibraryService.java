aa73e5e68fb15bb2d8f3869e9a9182ebc5a1683f
Frank <dev@goodow.com>
2012-06-22 18:45:36 +0800

    Refactor WebService

--- com.goodow.web/com.goodow.web.example/src/main/java/com/goodow/web/example/shared/LibraryService.java
+++ com.goodow.web/com.goodow.web.example/src/main/java/com/goodow/web/example/shared/LibraryService.java
@@ -1,9 +1,7 @@
 package com.goodow.web.example.shared;
 
 import com.goodow.web.core.shared.ContentService;
-import com.goodow.web.core.shared.WebService;
 
-@WebService
 public interface LibraryService extends ContentService<Library> {
 
   Library save(Library library, final boolean flag, final int size);
