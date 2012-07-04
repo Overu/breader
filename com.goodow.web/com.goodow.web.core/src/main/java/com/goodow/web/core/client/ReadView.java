6b216dd9e29fd79c371f547a47f62198d0924f85
fengjian <fengjian@goodow.com>
2012-07-04 10:37:11 +0800

    ScrollTest

--- com.goodow.web/com.goodow.web.core/src/main/java/com/goodow/web/core/client/ReadView.java
+++ com.goodow.web/com.goodow.web.core/src/main/java/com/goodow/web/core/client/ReadView.java
@@ -2,24 +2,18 @@ package com.goodow.web.core.client;
 
 import com.google.gwt.core.client.Scheduler;
 import com.google.gwt.core.client.Scheduler.ScheduledCommand;
-import com.google.gwt.user.client.Window;
 import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.HTMLPanel;
 
-import com.googlecode.mgwt.dom.client.event.touch.TouchCancelEvent;
-import com.googlecode.mgwt.dom.client.event.touch.TouchEndEvent;
-import com.googlecode.mgwt.dom.client.event.touch.TouchHandler;
-import com.googlecode.mgwt.dom.client.event.touch.TouchMoveEvent;
-import com.googlecode.mgwt.dom.client.event.touch.TouchStartEvent;
 import com.googlecode.mgwt.ui.client.widget.Button;
-import com.googlecode.mgwt.ui.client.widget.RoundPanel;
-import com.googlecode.mgwt.ui.client.widget.event.scroll.ScrollEndEvent;
-import com.googlecode.mgwt.ui.client.widget.touch.TouchPanel;
 
 public class ReadView extends DetailView {
 
-  TouchPanel container;
+  HTMLPanel container;
 
   Button computeBtn;
+
+  HTML html;
 
   public ReadView() {
 
@@ -30,44 +24,43 @@ public class ReadView extends DetailView {
 
     // main.add(carousel);
     main.ensureDebugId("read-main");
-    container = new RoundPanel();
+    container = new HTMLPanel("");
 
-    HTML html = new HTML();
+    html = new HTML();
 
     String content = "";
-    for (int i = 0; i < 100000; i++) {
-      content += " hello " + i;
+    for (int i = 0; i < 1000; i++) {
+      content += " a b c " + i;
     }
     html.setHTML(content);
 
     computeBtn = new Button("Compute");
-    main.add(computeBtn);
-    computeBtn.addTouchHandler(new TouchHandler() {
-
-      @Override
-      public void onTouchCanceled(final TouchCancelEvent event) {
-        // TODO Auto-generated method stub
-
-      }
-
-      @Override
-      public void onTouchEnd(final TouchEndEvent event) {
-        int w = container.getElement().getScrollWidth();
-        Window.alert("Width=" + w);
-      }
-
-      @Override
-      public void onTouchMove(final TouchMoveEvent event) {
-        // TODO Auto-generated method stub
-
-      }
-
-      @Override
-      public void onTouchStart(final TouchStartEvent event) {
-        // TODO Auto-generated method stub
-
-      }
-    });
+    // main.add(computeBtn);
+    // computeBtn.addTouchHandler(new TouchHandler() {
+    //
+    // @Override
+    // public void onTouchCanceled(final TouchCancelEvent event) {
+    // // TODO Auto-generated method stub
+    //
+    // }
+    //
+    // @Override
+    // public void onTouchEnd(final TouchEndEvent event) {
+    // int w = container.getElement().getScrollWidth();
+    // Window.alert("Width=" + w);
+    // }
+    //
+    // @Override
+    // public void onTouchMove(final TouchMoveEvent event) {
+    // // TODO Auto-generated method stub
+    //
+    // }
+    //
+    // @Override
+    // public void onTouchStart(final TouchStartEvent event) {
+    //
+    // }
+    // });
 
     scrollPanel.setSnap(true);
     scrollPanel.setMomentum(false);
@@ -78,15 +71,15 @@ public class ReadView extends DetailView {
 
     scrollPanel.setAutoHandleResize(true);
 
-    scrollPanel.addScrollEndHandler(new ScrollEndEvent.Handler() {
-
-      @Override
-      public void onScrollEnd(final ScrollEndEvent event) {
-
-        System.out.println("x=" + scrollPanel.getCurrentPageX());
-        System.out.println("y=" + scrollPanel.getCurrentPageY());
-      }
-    });
+    // scrollPanel.addScrollEndHandler(new ScrollEndEvent.Handler() {
+    //
+    // @Override
+    // public void onScrollEnd(final ScrollEndEvent event) {
+    //
+    // System.out.println("x=" + scrollPanel.getCurrentPageX());
+    // System.out.println("y=" + scrollPanel.getCurrentPageY());
+    // }
+    // });
     container.add(html);
     scrollPanel.setWidget(container);
 
@@ -103,6 +96,7 @@ public class ReadView extends DetailView {
   @Override
   protected void onAttach() {
     super.onAttach();
+    // container.setWidth(100000 + "px");
     Scheduler.get().scheduleDeferred(new ScheduledCommand() {
 
       @Override
@@ -113,12 +107,17 @@ public class ReadView extends DetailView {
   }
 
   private void adjustSize() {
-    int columnWidth = scrollPanel.getOffsetWidth() / 2;
+    int gap = 0;
+    int columnWidth = main.getOffsetWidth() - gap;
     int columnHeight = scrollPanel.getOffsetHeight();
     container.setHeight(columnHeight + "px");
+    // container.getElement().getStyle().setMargin(5, Unit.PX);
     container.getElement().getStyle().setProperty("webkitColumnWidth", columnWidth + "px");
+    container.getElement().getStyle().setProperty("webkitColumnGap", gap + "px");
     int scrollWidth = container.getElement().getScrollWidth();
     container.setWidth(scrollWidth + "px");
+    container.removeFromParent();
+    scrollPanel.setWidget(container);
     // Scheduler.get().scheduleDeferred(new ScheduledCommand() {
     // @Override
     // public void execute() {
