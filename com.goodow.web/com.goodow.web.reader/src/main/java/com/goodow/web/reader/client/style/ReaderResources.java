fd71d1eddfcc5dfc5f4ce334f74f8129a9773847
fengjian <fengjian@goodow.com>
2012-07-04 14:27:58 +0800

    add ReaderResources

---
+++ com.goodow.web/com.goodow.web.reader/src/main/java/com/goodow/web/reader/client/style/ReaderResources.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright 2012 Goodow.com
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package com.goodow.web.reader.client.style;
+
+import com.google.gwt.core.client.GWT;
+import com.google.gwt.resources.client.ClientBundle;
+import com.google.gwt.resources.client.CssResource;
+
+import java.util.logging.Logger;
+
+public class ReaderResources {
+
+  interface Bundle extends ClientBundle {
+    @Source("style.css")
+    Style css();
+  }
+
+  interface Style extends CssResource {
+
+  }
+
+  private static final Logger logger = Logger.getLogger(ReaderResources.class.getName());
+  private static Bundle INSTANCE;
+
+  static {
+    logger.finest("static init start");
+    INSTANCE = GWT.create(Bundle.class);
+    INSTANCE.css().ensureInjected();
+    logger.finest("static init end");
+  }
+
+  public static Bundle INSTANCE() {
+    return INSTANCE;
+  }
+
+}
