dd95dc5152688beb3363e6a6feb6682ff873d36f
fengjian <fengjian@goodow.com>
2012-07-04 13:28:55 +0800

    readview

--- com.goodow.web/com.goodow.web.core/src/main/java/com/goodow/web/core/client/ReadView.java
+++ com.goodow.web/com.goodow.web.core/src/main/java/com/goodow/web/core/client/ReadView.java
@@ -5,24 +5,15 @@ import com.google.gwt.core.client.Scheduler.ScheduledCommand;
 import com.google.gwt.user.client.ui.HTML;
 import com.google.gwt.user.client.ui.HTMLPanel;
 
-import com.googlecode.mgwt.ui.client.widget.Button;
+import com.googlecode.mgwt.ui.client.widget.event.scroll.ScrollEndEvent;
 
 public class ReadView extends DetailView {
 
   HTMLPanel container;
 
-  Button computeBtn;
-
   HTML html;
 
   public ReadView() {
-
-    // scrollPanel.removeFromParent();
-
-    // carousel = new Carousel();
-    // carousel.ensureDebugId("read-carousel");
-
-    // main.add(carousel);
     main.ensureDebugId("read-main");
     container = new HTMLPanel("");
 
@@ -34,34 +25,6 @@ public class ReadView extends DetailView {
     }
     html.setHTML(content);
 
-    computeBtn = new Button("Compute");
-    // main.add(computeBtn);
-    // computeBtn.addTouchHandler(new TouchHandler() {
-    //
-    // @Override
-    // public void onTouchCanceled(final TouchCancelEvent event) {
-    // // TODO Auto-generated method stub
-    //
-    // }
-    //
-    // @Override
-    // public void onTouchEnd(final TouchEndEvent event) {
-    // int w = container.getElement().getScrollWidth();
-    // Window.alert("Width=" + w);
-    // }
-    //
-    // @Override
-    // public void onTouchMove(final TouchMoveEvent event) {
-    // // TODO Auto-generated method stub
-    //
-    // }
-    //
-    // @Override
-    // public void onTouchStart(final TouchStartEvent event) {
-    //
-    // }
-    // });
-
     scrollPanel.setSnap(true);
     scrollPanel.setMomentum(false);
     scrollPanel.setShowScrollBarX(false);
@@ -71,32 +34,22 @@ public class ReadView extends DetailView {
 
     scrollPanel.setAutoHandleResize(true);
 
-    // scrollPanel.addScrollEndHandler(new ScrollEndEvent.Handler() {
-    //
-    // @Override
-    // public void onScrollEnd(final ScrollEndEvent event) {
-    //
-    // System.out.println("x=" + scrollPanel.getCurrentPageX());
-    // System.out.println("y=" + scrollPanel.getCurrentPageY());
-    // }
-    // });
+    scrollPanel.addScrollEndHandler(new ScrollEndEvent.Handler() {
+
+      @Override
+      public void onScrollEnd(final ScrollEndEvent event) {
+
+        System.out.println("x=" + scrollPanel.getCurrentPageX());
+        System.out.println("y=" + scrollPanel.getCurrentPageY());
+      }
+    });
     container.add(html);
     scrollPanel.setWidget(container);
-
-    // container.setHeight("400px");
-    // container.getElement().getStyle().setProperty("webkitColumnWidth", 500 + "px");
-    // container.setWidth("10000px");
   }
 
-  /*
-   * (non-Javadoc)
-   * 
-   * @see com.google.gwt.user.client.ui.Composite#onAttach()
-   */
   @Override
   protected void onAttach() {
     super.onAttach();
-    // container.setWidth(100000 + "px");
     Scheduler.get().scheduleDeferred(new ScheduledCommand() {
 
       @Override
@@ -107,23 +60,14 @@ public class ReadView extends DetailView {
   }
 
   private void adjustSize() {
-    int gap = 0;
-    int columnWidth = main.getOffsetWidth() - gap;
+    int columnWidth = main.getOffsetWidth();
     int columnHeight = scrollPanel.getOffsetHeight();
     container.setHeight(columnHeight + "px");
-    // container.getElement().getStyle().setMargin(5, Unit.PX);
     container.getElement().getStyle().setProperty("webkitColumnWidth", columnWidth + "px");
-    container.getElement().getStyle().setProperty("webkitColumnGap", gap + "px");
+    container.getElement().getStyle().setProperty("webkitColumnGap", "0px");
     int scrollWidth = container.getElement().getScrollWidth();
     container.setWidth(scrollWidth + "px");
     container.removeFromParent();
     scrollPanel.setWidget(container);
-    // Scheduler.get().scheduleDeferred(new ScheduledCommand() {
-    // @Override
-    // public void execute() {
-    // int scrollWidth = container.getElement().getScrollWidth();
-    // container.setWidth(scrollWidth + "px");
-    // }
-    // });
   }
 }
