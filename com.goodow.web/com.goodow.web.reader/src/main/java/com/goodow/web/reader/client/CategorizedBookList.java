1e3b0eef0d2e674384b23790d9cc3952eb9eaa4f
fengjian <fengjian@goodow.com>
2012-07-13 17:00:09 +0800

    change CategorizedBookList

--- com.goodow.web/com.goodow.web.reader/src/main/java/com/goodow/web/reader/client/CategorizedBookList.java
+++ com.goodow.web/com.goodow.web.reader/src/main/java/com/goodow/web/reader/client/CategorizedBookList.java
@@ -3,62 +3,55 @@ package com.goodow.web.reader.client;
 import com.goodow.web.reader.client.style.ReadResources;
 import com.goodow.web.reader.shared.Book;
 
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.HTMLPanel;
 import com.google.inject.Inject;
 import com.google.inject.Provider;
 
 import com.googlecode.mgwt.ui.client.widget.ScrollPanel;
-import com.googlecode.mgwt.ui.client.widget.event.scroll.ScrollMoveEvent;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class CategorizedBookList extends AbstractBookList {
+public class CategorizedBookList extends Composite {
+
+  HTMLPanel bookList;
+
+  HTMLPanel container;
 
   CategoryListView categoryListView;
 
   @Inject
   public CategorizedBookList(final Provider<BookSummary> bookSummaryProvider) {
-    super(bookSummaryProvider);
 
-    setScrollingEnabledX(false);
-    setScrollingEnabledY(false);
-    setShowScrollBarX(false);
-    setShowScrollBarY(false);
+    container = new HTMLPanel("");
+    bookList = new HTMLPanel("");
+    categoryListView = new CategoryListView();
 
     container.addStyleName(ReadResources.INSTANCE().categroyListCss().categorContainer());
 
-    categoryListView = new CategoryListView();
-    categoryListView.addScrollMoveHandler(new ScrollMoveEvent.Handler() {
+    for (Book book : createBooks()) {
+      BookSummary view = bookSummaryProvider.get();
+      view.setLandscape(setBookSummaryLandscapeCss());
+      view.setBook(book);
+      bookList.add(view);
+    }
 
-      @Override
-      public void onScrollMove(final ScrollMoveEvent event) {
-        event.getEvent().stopPropagation();
-      }
-    });
-
-    bookList.removeFromParent();
-    ScrollPanel scrollPanel2 = new ScrollPanel();
-    scrollPanel2.setWidget(bookList);
-    scrollPanel2.setScrollingEnabledX(false);
-    scrollPanel2.setScrollingEnabledY(true);
-    scrollPanel2.addScrollMoveHandler(new ScrollMoveEvent.Handler() {
-
-      @Override
-      public void onScrollMove(final ScrollMoveEvent event) {
-        event.getEvent().stopPropagation();
-      }
-    });
+    ScrollPanel scrollPanel = new ScrollPanel();
+    scrollPanel.setWidget(bookList);
+    scrollPanel.setScrollingEnabledX(false);
+    scrollPanel.setScrollingEnabledY(true);
 
     container.add(categoryListView);
-    container.add(scrollPanel2);
+    container.add(scrollPanel);
+
+    initWidget(container);
   }
 
-  @Override
   public String setBookSummaryLandscapeCss() {
     return BookSummary.INSTANCE.landscape().root1();
   }
 
-  @Override
   protected List<Book> createBooks() {
     List<Book> result = new ArrayList<Book>();
     for (int i = 0; i < 40; i++) {
